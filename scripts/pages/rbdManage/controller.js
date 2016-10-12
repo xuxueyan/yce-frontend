6293c7c9b44211f1150158205efd30b81c5faef3
xuxueyan <2425412794@qq.com>
2016-10-12 15:59:30 +0800

    创建用户

--- scripts/pages/rbdManage/controller.js
+++ scripts/pages/rbdManage/controller.js
@@ -6,14 +6,32 @@ define([
     ], function(Base64){
         'use strict';
 
-        var ctrl = ['$scope', function($scope){
+        var ctrl = ['$scope', '$http', 'rbdManageService', '$localStorage', function($scope,$http,rbdManageService,$localStorage){
+
+            var param = {
+                "Authorization" : $localStorage.sessionId
+            }
+
+            rbdManageService.setUpUser('param',function(res){
+                console.log(angular.toJson(res.data)+"#####  0--");
+                $scope.activities = JSON.parse(res.data);
+                $scope.setUsername = function(){
+                    // rbdManageService.setUsernamePost('param',function(number){
+
+                        console.log("失焦事件啦~~");
+                    //     console.log(angular.toJson(number));
+                    // })
+                }
+            })
 
         }];
 
 
 
         var controllers = [
-            {module: 'rbdManage', name: 'rbdManageController', ctrl: ctrl}
+            {module: 'rbdManage',
+            name: 'rbdManageController',
+            ctrl: ctrl}
         ];
 
         return controllers;
--- scripts/pages/rbdManage/service.js
+++ scripts/pages/rbdManage/service.js
@@ -6,6 +6,15 @@ define([
 	var getApis = function($http){
 		var apis = {};
 
+
+		apis.setUpUser = function(param, success, error){
+			return utils.http($http, 'get', '/api/v1/user/init', param, success, error)
+		}
+
+		apis.setUsernamePost = function(param, success, error){
+			return utils.http($http, 'post', '/api/v1/user/check', param, success, error)
+		}
+
 		return apis;
 	};	
 
