8f4303fa46c92bfa9c1bb725306b5a89f79b805c
markh5 <markh5@qq.com>
2016-10-24 15:44:22 +0800

    loadApplist 1s

--- scripts/pages/appManage/controller.js
+++ scripts/pages/appManage/controller.js
@@ -90,7 +90,9 @@ define([
                 $scope.canSubmit = false;
                 appManageService.submitRollingup(param, function(data) {
                     $rootScope.widget.widgetRollingup = false;
-                    $scope.loadAppList();
+                    $timeout(function(){
+                        $scope.loadAppList();
+                    },1000);
                     $scope.canSubmit = true;
 
                 }, function() {
@@ -168,7 +170,9 @@ define([
                 $scope.canSubmit = false;
                 appManageService.submitScale(param, function(rep) {
                     $rootScope.widget.widgetScale = false;
-                    $timeout
+                    $timeout(function(){
+                        $scope.loadAppList();
+                    },1000);
                     $scope.canSubmit = true;
                 }, function() {
                     $scope.canSubmit = true;
@@ -195,7 +199,9 @@ define([
                 $scope.canSubmit = false;
                 appManageService.submitDelete($scope.param, function(data) {
                     $rootScope.widget.widgetDelete = false;
-                    $scope.loadAppList();
+                    $timeout(function(){
+                        $scope.loadAppList();
+                    },1000);
                     $scope.canSubmit = true;
                 }, function() {
                     $scope.canSubmit = true;
